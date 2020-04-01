58c1579f0b4f518c15793793cdbac00116addc59
Kally <kallyasmedia@gmail.com>
2020-04-01 14:00:36 +0000

    adde remaining html code

--- main.js
+++ main.js
@@ -1,57 +1,22 @@
-//Js basics
-// DOM Elements
+const themeMap = {
+  dark: "light",
+  light: "solar",
+  solar: "dark"
+};
 
-const darkButton = document.getElementById('dark');
-const lightButton = document.getElementById('light');
-const solarButton = document.getElementById('solar');
-const body = document.body;
+const theme = localStorage.getItem('theme')
+  || (tmp = Object.keys(themeMap)[0],
+      localStorage.setItem('theme', tmp),
+      tmp);
+const bodyClass = document.body.classList;
+bodyClass.add(theme);
 
+function toggleTheme() {
+  const current = localStorage.getItem('theme');
+  const next = themeMap[current];
 
-// Apply the cached theme on reload
-
-const theme = localStorage.getItem('theme');
-const isSolar = localStorage.getItem('isSolar');
-
-if (theme) {
-  body.classList.add(theme);
-  isSolar && body.classList.add('solar');
+  bodyClass.replace(current, next);
+  localStorage.setItem('theme', next);
 }
 
-// Button Event Handlers
-
-darkButton.onclick = () => {
-  body.classList.replace('light', 'dark');
-  localStorage.setItem('theme', 'dark');
-};
-
-lightButton.onclick = () => {
-  body.classList.replace('dark', 'light');
-
-  localStorage.setItem('theme', 'light');
-};
-
-solarButton.onclick = () => {
-
-  if (body.classList.contains('solar')) {
-    
-    body.classList.remove('solar');
-    solarButton.style.cssText = `
-      --bg-solar: var(--yellow);
-    `
-
-    solarButton.innerText = 'solarize';
-
-    localStorage.removeItem('isSolar');
-
-  } else {
-
-    solarButton.style.cssText = `
-      --bg-solar: white;
-    `
-
-    body.classList.add('solar');
-    solarButton.innerText = 'normalize';
-
-    localStorage.setItem('isSolar', true);
-  }
-};
+document.getElementById('themeButton').onclick = toggleTheme;
