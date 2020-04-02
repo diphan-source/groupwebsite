fa3f82df7dea6d6e20cd45247522933213bb1c4f
Kally <kallyasmedia@gmail.com>
2020-04-02 11:02:48 +0000

    added a calculator

--- calculator.js
+++ calculator.js
@@ -19,21 +19,54 @@ In this case, we can use an event delegation pattern to listen,
 //grab elemnts from the DOM
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculator_keys');
-const action = key.dataset.action;
-
-//If the key does not have a data-action attribute, it must be a number key.
-if(!action) {
-    console.log('Number key!')
-};
-
-if(action === 'add' || action === 'subtract' || action === 'divide' || action === 'multiply') {
-    console.log("Operator Key!")
-}
+const display = document.querySelector('.calculator_display')
 
 //add event listeners
 keys.addEventListener('click', e => {
     if(e.target.matches('button')) {
-        const key = e.target;
+        const key = e.target
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = display.textContent
+        const previousKeyType = calculator.dataset.previousKeyType
+         Array.from(key.parentNode.children)
+      .forEach(k => k.classList.remove('is-depressed'))
+
+    if (!action) {
+      if (displayedNum === '0' || previousKeyType === 'operator') {
+        display.textContent = keyContent
+      } else {
+        display.textContent = displayedNum + keyContent
+      }
+    }
+
+    if (action === 'decimal') {
+      display.textContent = displayedNum + '.'
+    }
+
+    if (
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'
+    ) {
+      key.classList.add('is-depressed')
+      calculator.dataset.previousKeyType = 'operator'
+      calculator.dataset.firstValue = displayedNum
+      calculator.dataset.operator = action
+    }
+
+    if (action === 'clear') {
+      console.log('clear key!')
+    }
+
+    if (action === 'calculate') {
+      const firstValue = calculator.dataset.firstValue
+      const operator = calculator.dataset.operator
+      const secondValue = displayedNum
+
+      display.textContent = calculate(firstValue, operator, secondValue)
+    }
 
     }
 })
