b682e6ff20422eb336f6d826f99c7fa8b9df2347
Kally <kallyasmedia@gmail.com>
2020-04-02 11:04:21 +0000

    added a calculator

--- calculator.js
+++ calculator.js
@@ -70,3 +70,18 @@ keys.addEventListener('click', e => {
 
     }
 })
+
+const calculate = (n1, operator, n2) => {
+  let result = ''
+  if (operator === 'add') {
+    result = parseFloat(n1) + parseFloat(n2)
+  } else if (operator === 'subtract') {
+    result = parseFloat(n1) - parseFloat(n2)
+  } else if (operator === 'multiply') {
+    result = parseFloat(n1) * parseFloat(n2)
+  } else if (operator === 'divide') {
+    result = parseFloat(n1) / parseFloat(n2)
+  }
+
+  return result
+}
